db8bfa0ef537f30ad82f762a3275e43c42291286
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-08-19 07:07:36 +0530

    Add files via upload

---
+++ Circles/variety_circle.py
@@ -0,0 +1,12 @@
+from turtle import*
+clr=["black","violet","purple","red","pink","blue","yellow","orange"]
+pensize(3)
+for n in clr:
+	color("black",n)
+	begin_fill()
+	circle(60)
+	circle(70)
+	circle(80)
+	circle(90)
+	end_fill()
+	rt(45)
